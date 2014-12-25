f43cce0269aad50a2600fa466978197689045957
Sandeep Mukherjee <mukherjee.sandeep@gmail.com>
2014-12-25 02:00:05 +0000

    Some code refactoring.

--- wificar.py
+++ wificar.py
@@ -10,19 +10,40 @@ import fileinput
 import re
 
 
-PIN_LEFT = 23
-PIN_RIGHT = 24
+PIN_LEFT = 24 # Turn this on to move the left track.
+PIN_RIGHT = 23 # Turn this on to move the right track.
 
-def forward(duration):
+def pulse(pin, duration_on, duration_off, endtime):
+    while endtime > time.time():
+        GPIO.output(pin, False)
+        time.sleep(duration_off)
+        GPIO.output(pin, True)
+        time.sleep(duration_on)
+
+# pin_corr must be one of PIN_RIGHT or PIN_LEFT.
+# If rover deviates to right, use PIN_LEFT
+# Otherwise use PIN_RIGHT.
+# corr_on is duration of time pin_corr remains on.
+# corr_off is the duration it remains off.
+# Smaller corr_on results in larger correction.
+def forward(duration, pin_corr, corr_on):
     print "Forward"
-    GPIO.output(PIN_RIGHT, True)
+    if pin_corr != PIN_LEFT and pin_corr != PIN_RIGHT:
+        raise RuntimeError("Invalid pin_corr")
+    
+    pin_const = PIN_RIGHT
+    if pin_corr == PIN_RIGHT:
+        pin_const = PIN_LEFT
+
+    GPIO.output(pin_const, True)
     starttime = time.time()    
     endtime = starttime + duration
-    while endtime > time.time():
-        GPIO.output(PIN_LEFT, False)
-        time.sleep(0.1)
-        GPIO.output(PIN_LEFT, True)
-        time.sleep(0.25)
+
+    if corr_on < 0 or corr_on > 1.0:
+        raise RuntimeError("corr_on must be between 0 and 1.0")
+    corr_off = 1.0 - corr_on
+    pulse(pin_corr, corr_on, corr_off, endtime)
+
 
 def stop():
     print "Stop"
@@ -37,25 +58,26 @@ def cleanup():
 
 def left(duration):
     print "Left turn"
-    GPIO.output(PIN_LEFT, True)
-    # Keep right moving, but slowly
-    starttime = time.time()    
-    endtime = starttime + duration
-    while endtime > time.time():
-        GPIO.output(PIN_RIGHT, False)
-        time.sleep(0.1)
-        GPIO.output(PIN_RIGHT, True)
-        time.sleep(0.1)
-
-def right(duration):
-    print "Right turn"
+    # Keep the right belt moving, and slow down the left one.
     GPIO.output(PIN_RIGHT, True)
     starttime = time.time()    
     endtime = starttime + duration
     while endtime > time.time():
         GPIO.output(PIN_LEFT, False)
-        time.sleep(0.3)
+        time.sleep(0.2)
         GPIO.output(PIN_LEFT, True)
+        time.sleep(0.1)
+
+def right(duration):
+    print "Right turn"
+    # Keep left belt moving, slow down the right one.
+    GPIO.output(PIN_LEFT, True)
+    starttime = time.time()    
+    endtime = starttime + duration
+    while endtime > time.time():
+        GPIO.output(PIN_RIGHT, False)
+        time.sleep(0.3)
+        GPIO.output(PIN_RIGHT, True)
         time.sleep(0.1)
 
 GPIO.setmode(GPIO.BOARD)
@@ -77,5 +99,5 @@ for line in fileinput.input():
             stop()
             time.sleep(delta)
         elif command == "f":
-            forward(delta)
+            forward(delta, PIN_LEFT, 0.85)
 
